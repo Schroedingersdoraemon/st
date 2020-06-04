fcf320e2fb280ca39414445a0ee3463a983db7da
Schroedingersdoraemon <1181822466@qq.com>
2020-06-04 16:00:18 +0800

    added focus opacity functions, works well

--- config.h
+++ config.h
@@ -92,7 +92,8 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.95;
+float alpha = 0.8;           //< alpha value used when the window is focused.
+float alphaUnfocussed = 0.65; //< alpha value used when the focus is lost
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -121,13 +122,15 @@ static const char *colorname[] = {
   /* special colors */
   [256] = "#282a36", /* background */
   [257] = "#f8f8f2", /* foreground */
+  [258] = "black",
 };
 /*
  * Default colors (colorname index)
  * foreground, background, cursor
  */
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
+//unsigned int defaultbg = 256;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 256;
 
--- st.h
+++ st.h
@@ -129,3 +129,5 @@ extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
 extern float alpha;
+extern float alphaUnfocussed;
+extern float alpha;
