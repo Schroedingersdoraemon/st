1e79813c988c447640fde0419d55404379e6e4be
Schroedingersdoraemon <1181822466@qq.com>
2020-11-04 17:51:37 +0800

    added MS KaiTi

--- config.h
+++ config.h
@@ -10,6 +10,7 @@ static char *font = "SauceCodePro Nerd Font Mono:pixelsize=28:antialias=true:aut
 static char *font2[] = {
 	//"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
 	//"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
+    "KaiTi-Regular:pixelsize=28:antialias=true:autohint=true"
     "JoyPixels:pixelsize=19:antialias=true:autohint=true"
 };
 
